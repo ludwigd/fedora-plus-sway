7cb654de2dadeff1199e787100d708a2cb97eb02
Damian Ludwig <mail@damianludwig.org>
2023-07-21 14:24:28 +0200

    split the script into smaller (and selectable) tasks

--- install.sh
+++ install.sh
@@ -1,213 +1,309 @@
 #!/bin/bash
-set -euo  pipefail
+set -eo  pipefail
 
-# Only root can run this script
-[ $UID -eq 0 ] || exit 1
+# Wifi-support and basic networking tools
+subtask_network () {
+    dnf -y install \
+	bluez \
+	iwlwifi-mvm-firmware \
+	NetworkManager-openvpn \
+	NetworkManager-openvpn-gnome \
+	NetworkManager-wifi \
+	nm-connection-editor \
+	nm-connection-editor-desktop
+}
 
-# Ensure the system is up to date
-dnf update -y --refresh
+# Install pipewire along with some tools
+subtask_audio () {
+    dnf -y install \
+	pavucontrol \
+	pipewire \
+	pipewire-pulseaudio \
+	playerctl \
+	pulseaudio-utils
+}
 
-# Handy standard tools
-dnf -y install \
-    @Standard \
-    vim-enhanced
-
-# Basic Desktop
-dnf -y install \
-    clipman \
-    foot \
-    gammastep \
-    grim \
-    i3status \
-    jq \
-    kanshi \
-    light \
-    mate-polkit \
-    python3-i3ipc \
-    sway \
-    wev \
-    wofi \
-    xlsclients \
-    xorg-x11-server-Xwayland
+# Printing support
+subtask_printing () {
+    dnf -y install \
+	cups \
+	ghostscript \
+	hplip \
+	system-config-printer \
+	--exclude PackageKit,PackageKit-glib
+}
 
 # Fonts
-dnf -y install \
-    dejavu-sans-fonts \
-    dejavu-sans-mono-fonts \
-    dejavu-serif-fonts \
-    fontawesome-fonts \
-    fontconfig \
-    google-noto-emoji-color-fonts \
-    google-noto-sans-cjk-ttc-fonts \
-    jetbrains-mono-fonts-all \
-    liberation-mono-fonts \
-    liberation-sans-fonts \
-    liberation-serif-fonts
+subtask_fonts () {
+    dnf -y install \
+	dejavu-sans-fonts \
+	dejavu-sans-mono-fonts \
+	dejavu-serif-fonts \
+	fontawesome-fonts \
+	fontconfig \
+	google-noto-emoji-color-fonts \
+	google-noto-sans-cjk-ttc-fonts \
+	jetbrains-mono-fonts-all \
+	liberation-mono-fonts \
+	liberation-sans-fonts \
+	liberation-serif-fonts
+}
 
-# Audio, Bluetooth and Network
-dnf -y install \
-    bluez \
-    gnome-keyring \
-    gnome-keyring-pam \
-    iwlwifi-mvm-firmware \
-    NetworkManager-openvpn \
-    NetworkManager-openvpn-gnome \
-    NetworkManager-wifi \
-    nm-connection-editor \
-    nm-connection-editor-desktop \
-    openssl \
-    pavucontrol \
-    pipewire \
-    pipewire-pulseaudio \
-    playerctl \
-    pulseaudio-utils
+# Very basic desktop, just sway and vim along with audio and wifi support
+task_basic_desktop () {
+    subtask_network
+    subtask_audio
+    
+    dnf -y install \
+	@Standard \
+	sway \
+	vim-enhanced
+}
 
-# Printing
-dnf -y install \
-    cups \
-    ghostscript \
-    hplip \
-    system-config-printer \
-    --exclude PackageKit,PackageKit-glib
+# An enhanced desktop environment
+task_enhanced_desktop () {
+    task_basic_desktop
+    subtask_fonts
+    subtask_printing
+    
+    dnf -y install \
+	adwaita-gtk2-theme \
+	adwaita-icon-theme \
+	clipman \
+	gammastep \
+	gnome-icon-theme \
+	gnome-keyring \
+	gnome-keyring-pam \
+	gnome-themes-extra \
+	i3status \
+	jq \
+	kanshi \
+	light \
+	lxmenu-data \
+	mate-polkit \
+	pcmanfm \
+	python3-i3ipc \
+	rofi \
+	wev \
+	xlsclients
 
-# Extra packages
-dnf -y install \
-    adwaita-gtk2-theme \
-    adwaita-icon-theme \
-    android-file-transfer \
-    android-tools \
-    borgbackup \
-    distrobox \
-    emacs \
-    fish \
-    flatpak \
-    gimp \
-    gnome-icon-theme \
-    gnome-themes-extra \
-    htop \
-    imv \
-    inkscape \
-    lxmenu-data \
-    lynx \
-    mutt \
-    pcmanfm \
-    podman \
-    powertop \
-    sshfs \
-    virt-manager \
-    xournalpp \
-    xsane \
-    zathura \
-    zathura-plugins-all \
-    zathura-fish-completion
+    dnf -y copr enable ludwigd/rofi-mode-sway
+    dnf -y install \
+	rofi-mode-sway
+}
 
-# (Kernel) Development Tools
-dnf -y install \
-    autoconf \
-    automake \
-    bc \
-    binutils \
-    bison \
-    cargo \
-    cmake \
-    ctags \
-    dwarves \
-    elfutils-libelf-devel \
-    flex \
-    gcc \
-    gcc-c++ \
-    gdb \
-    git \
-    git-email \
-    glibc-devel \
-    java-latest-openjdk \
-    java-latest-openjdk-devel \
-    javacc \
-    make \
-    ncurses-devel \
-    openssl-devel \
-    patch \
-    patchutils \
-    python3 \
-    python3-pip \
-    python3-virtualenv \
-    rust \
-    strace \
-    zstd
+# Extra desktop apps
+task_apps () {
+    local flatpak=$1
+    
+    dnf -y install \
+	android-file-transfer \
+	android-tools \
+	borgbackup \
+	distrobox \
+	emacs \
+	fish \
+	htop \
+	imv \
+	lynx \
+	mutt \
+	podman \
+	powertop \
+	sshfs \
+	virt-manager \
+	xsane \
+	zathura \
+	zathura-plugins-all \
+	zathura-fish-completion
+
+    if [[ -z "$flatpak" ]]; then
+	dnf -y install \
+	    chromium \
+	    ffmpeg-free \
+	    firefox \
+	    gimp \
+	    gstreamer1-plugin-libav \
+	    inkscape \
+	    libreoffice \
+	    libreoffice-x11 \
+	    mpv \
+	    quodlibet \
+	    xournalpp
+    elif [[ $flatpak == "flatpak" ]]; then
+	dnf -y install flatpak
+	
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak remote-modify --enable flathub
+	
+	flatpak install -y flathub \
+		com.github.xournalpp.xournalpp \
+		io.github.quodlibet.QuodLibet \
+		io.mpv.Mpv \
+		org.chromium.Chromium \
+		org.gimp.GIMP \
+		org.inkscape.Inkscape \
+		org.libreoffice.LibreOffice \
+		org.mozilla.firefox
+	
+	flatpak override \
+		--env=MOZ_ENABLE_WAYLAND=1 \
+		org.mozilla.firefox
+    else
+	echo "Unknown parameter '$flatpak'. Must be empty or 'flatpak'."
+	exit 1
+    fi
+}
+
+# Development Tools
+task_development () {
+    dnf -y install \
+	autoconf \
+	automake \
+	bc \
+	binutils \
+	bison \
+	cargo \
+	cmake \
+	ctags \
+	flex \
+	gcc \
+	gcc-c++ \
+	gdb \
+	git \
+	glibc-devel \
+	java-latest-openjdk \
+	java-latest-openjdk-devel \
+	javacc \
+	make \
+	patch \
+	patchutils \
+	python3 \
+	python3-pip \
+	python3-virtualenv \
+	rust \
+	strace \
+	zstd
+}
 
 # TeXlive and publishing
-dnf -y install \
-    aspell \
-    aspell-de \
-    aspell-en \
-    hunspell \
-    hunspell-de \
-    ImageMagick \
-    pandoc \
-    texlive-collection-basic \
-    texlive-collection-bibtexextra \
-    texlive-collection-binextra \
-    texlive-collection-context \
-    texlive-collection-fontsextra \
-    texlive-collection-fontsrecommended \
-    texlive-collection-fontutils \
-    texlive-collection-formatsextra \
-    texlive-collection-langenglish \
-    texlive-collection-langgerman \
-    texlive-collection-latex \
-    texlive-collection-latexextra \
-    texlive-collection-latexrecommended \
-    texlive-collection-luatex \
-    texlive-collection-mathscience \
-    texlive-collection-metapost \
-    texlive-collection-pictures \
-    texlive-collection-plaingeneric \
-    texlive-collection-pstricks \
-    texlive-collection-publishers \
-    texlive-collection-xetex \
-    texstudio \
-    xfig \
-    --exclude evince
+task_publishing () {
+    dnf -y install \
+	aspell \
+	aspell-de \
+	aspell-en \
+	hunspell \
+	hunspell-de \
+	ImageMagick \
+	pandoc \
+	texlive-collection-basic \
+	texlive-collection-bibtexextra \
+	texlive-collection-binextra \
+	texlive-collection-context \
+	texlive-collection-fontsextra \
+	texlive-collection-fontsrecommended \
+	texlive-collection-fontutils \
+	texlive-collection-formatsextra \
+	texlive-collection-langenglish \
+	texlive-collection-langgerman \
+	texlive-collection-latex \
+	texlive-collection-latexextra \
+	texlive-collection-latexrecommended \
+	texlive-collection-luatex \
+	texlive-collection-mathscience \
+	texlive-collection-metapost \
+	texlive-collection-pictures \
+	texlive-collection-plaingeneric \
+	texlive-collection-pstricks \
+	texlive-collection-publishers \
+	texlive-collection-xetex \
+	texstudio \
+	xfig \
+	--exclude evince
+}
 
-# Add and enable Flathub
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-modify --enable flathub
+# Dotfiles
+task_dotfiles () {
+    echo "Not implemented"
+    exit 1
+}
 
-# A word about flatpaks: While I prefer distro-provided packages, some
-# are not available in Fedora (mpv, Bitwarden) or are not working as
-# intended because of missing codecs, e.g., Quodlibet, Firefox,
-# Chromium. LibreOffice is installed as a flatpak because I do not
-# trust the documents I have to process with it and hence reduce the
-# risk of local compromise by sandboxing LibreOffice. Okular, when
-# installed as RPM, would draw in a lot of KDE dependencies. As I use
-# it rarely and only for annotating PDFs (otherwise I use Zathura),
-# the Flatpak comes in handy.
+usage () {
+    echo "install.sh <task>"
+    echo "  This script installs my Fedora+Sway environment."
 
-# Install some Flatpaks ...
-flatpak install -y flathub \
-    com.bitwarden.desktop \
-    io.github.quodlibet.QuodLibet \
-    io.mpv.Mpv \
-    org.chromium.Chromium \
-    org.kde.okular \
-    org.libreoffice.LibreOffice \
-    org.mozilla.firefox
+    echo -e "\\nTasks:"
+    echo "  basic                   - just sway and vim (incl. audio and wifi support)"
+    echo "  enhanced                - an enhanced basic environment"
+    echo "  apps [flatpak]          - desktop apps"
+    echo "  development             - some programming languages and tools"
+    echo "  publishing              - an opiniated selection of TeXlive collections and tools"
+    echo "  everything [flatpak]    - enhanced + apps +  development + publishing"
+    echo "  dotfiles                - install dotfiles (requires git)"
+    
+    echo -e "\\nApplications affected by the flatpak parameter:"
+    echo "  Chromium"
+    echo "  Firefox"
+    echo "  GIMP"
+    echo "  Inkscape"
+    echo "  LibreOffice"
+    echo "  mpv"
+    echo "  Quodlibet"
+    echo "  Xournal++"
+}
 
-# ... and alter them
-flatpak override \
-    --unshare=network \
-    --nosocket=pulseaudio \
-    --socket=cups \
-    --nofilesystem=host \
-    --filesystem=home \
-    org.libreoffice.LibreOffice
+assure_root () {
+    if [ $UID -ne 0 ]; then
+	echo "You must be root to install software."
+	exit 1
+    fi
+}
 
-flatpak override \
-    --env=MOZ_ENABLE_WAYLAND=1 \
-    org.mozilla.firefox
+update_system () {
+    dnf update -y --refresh
+}
 
-# Rebuild initramfs
-dracut -f
+main () {
+    local cmd=$1
 
-# Restart
-systemctl reboot
+    if [[ -z "$cmd" ]]; then
+	usage
+    elif [[ $cmd == "basic" ]]; then
+	assure_root
+	update_system
+	task_basic_desktop
+    elif [[ $cmd == "enhanced" ]]; then
+	assure_root
+	update_system
+	task_enhanced_desktop
+    elif [[ $cmd == "apps" ]]; then
+	assure_root
+	update_system
+	task_apps "$2"
+    elif [[ $cmd == "development" ]]; then
+	assure_root
+	update_system
+	task_development
+    elif [[ $cmd == "publishing" ]]; then
+	assure_root
+	update_system
+	task_publishing
+    elif [[ $cmd == "everything" ]]; then
+	assure_root
+	update_system
+	task_enhanced_desktop
+	task_apps "$2"
+	task_development
+	task_publishing
+    elif [[ $cmd == "dotfiles" ]]; then
+	if [ $UID -ne 0 ]; then
+	    task_dotfiles
+	else
+	    echo "You should NOT be root for this task."
+	    exit 1
+	fi
+    else
+	usage
+    fi
+}
+
+main "$@"
